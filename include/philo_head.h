e0ae1a4a47a05cc4dd302b8b4005472861d080cb
Myroniuk Oleksandr <moleksan@c2r11s6.42wolfsburg.de>
2025-03-28 15:16:39 +0100

    chngd_debug_func

--- include/philo_head.h
+++ include/philo_head.h
@@ -17,34 +17,32 @@
 #define SAD_FACE         "\U0001F622"
 #define RESET "\033[0m"
 
-
-/* Структура для хранения всех параметров симуляции */
 typedef struct s_simulation
 {
-    pthread_mutex_t *log_mutex;        // Мьютекс для вывода
-    pthread_mutex_t *forks;            // Массив мьютексов для вилок
-    int             sim_ready;  // Индикатор готовности симуляции
-    int             time_tdie;       // Время до смерти философа без еды
-    int             time_tsleep;     // Время сна
-    int             time_teat;       // Время, которое требуется на еду
-    int             meals_reqrd;    // Необходимое количество приёмов пищи
-    int             sim_end;    // Флаг окончания симуляции
-    int             ttl_philo;      // Общее количество философов
-    int             all_full;          // Флаг, указывающий, что все философы наелись
-    int             meals_eaten;       // Счётчик съеденных приемов пищи
+    pthread_mutex_t *log_mutex;  
+    pthread_mutex_t *forks;   
+    int             sim_ready; 
+    int             time_tdie;      
+    int             time_tsleep;    
+    int             time_teat;      
+    int             meals_reqrd;   
+    int             sim_end;   
+    int             ttl_philo;    
+    int             all_full;         
+    int             meals_eaten;      
 }               t_sim;
 
-/* Структура для представления каждого философа */
+
 typedef struct s_individual
 {
-    pthread_t       thread_id;      // Поток, связанный с философом
-    pthread_mutex_t *lt_fork;     // Ссылка на левую вилку
-    pthread_mutex_t *rt_fork;    // Ссылка на правую вилку
-    int             philo_id;       // Уникальный идентификатор философа
-    int             times_eaten;    // Количество приемов пищи
-    long int        start_time;     // Начальное время симуляции
-    long int        last_meal;      // Время последнего приема пищи
-    t_sim    *shared_data;   // Указатель на общую структуру симуляции
+    pthread_t       thread_id;   
+    pthread_mutex_t *lt_fork;     
+    pthread_mutex_t *rt_fork;    
+    int             philo_id;      
+    int             times_eaten;    
+    long int        start_time;     
+    long int        last_meal;     
+    t_sim    *shared_data;   
 }               t_indv;
 
 // validation_args
--- main/tools.c
+++ main/tools.c
@@ -6,64 +6,9 @@
 /*   By: moleksan <moleksan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 13:30:11 by moleksan          #+#    #+#             */
-/*   Updated: 2025/03/28 15:00:32 by moleksan         ###   ########.fr       */
+/*   Updated: 2025/03/28 15:14:28 by moleksan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo_head.h"
 
-// void ft_my_sleep(uint64_t duration)
-// {
-//     struct timeval tv;
-//     uint64_t time_start, time_elapsed;
-
-//     // Получаем текущее время в миллисекундах
-//     gettimeofday(&tv, NULL);
-//     time_start = tv.tv_sec * 1000 + tv.tv_usec / 1000;
-
-//     // Засыпаем до тех пор, пока не пройдет нужное количество времени
-//     while (1)
-//     {
-//         usleep(150);  // Немного приостанавливаем выполнение
-//         gettimeofday(&tv, NULL);
-//         time_elapsed = tv.tv_sec * 1000 + tv.tv_usec / 1000;
-
-//         if (time_elapsed - time_start >= duration)
-//             break;
-//     }
-// }
-
-// static long int	time_ms(void)
-// {
-// 	struct timeval	tv;
-
-// 	gettimeofday(&tv, NULL);
-// 	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
-// }
-
-// // sleeps for the time, indicated by variable time in ms
-// // usleep > 10 leads 2 ft_msleep  delays, 1 ms for every 200 ms twice in 5 cases
-// void	ft_msleep(long int time)
-// {
-// 	long int	start;
-
-// 	start = time_ms();
-// 	while (1)
-// 	{
-// 		usleep(150);
-// 		if (time_ms() - start >= time)
-// 			break ;
-// 	}
-// }
-
-// // returns ms passed since the time given as the argument
-// long	get_time(struct timeval time)
-// {
-// 	struct timeval	now;
-// 	int				diff;
-
-// 	gettimeofday(&now, NULL);
-// 	diff = (now.tv_sec * 1000 + now.tv_usec / 1000) - (time.tv_sec * 1000
-// 			+ time.tv_usec / 1000);
-// 	return (diff);
-// }
